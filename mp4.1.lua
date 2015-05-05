ef58bc7db1bb82ccd901ef7c110c5cc3f0b1e9b2
AeroHand <ketian.zhang@163.com>
2015-05-04 22:41:12 -0500

    still debugging
    d

--- mp4.1.lua
+++ mp4.1.lua
@@ -18,9 +18,12 @@ function getFile(file_name)
     end
   end    
   policy={}
+  ne={}
   for i=0,5 do
   	policy[i]={}
+  	ne[i]={}
     for j=0,5 do
+    	ne[i][j]=0
       a[i][j]=f:read("*number")
       policy[i][j]=" wall"
       if a[i][j]~=2 then
@@ -47,6 +50,13 @@ function get(ttt)
    end	 
 end
 
+function trueval(ttt)
+  if ttt==2 then
+  	return 0
+  else
+    return ttt
+  end  	
+end
 function bell()
   local tempval={}
   for i=0,5 do
@@ -80,11 +90,42 @@ function bell()
   end  
 end	
 
+function notwall(aa,bb,d)
+  fa=aa
+  fb=bb
+  if d==1 then     --up
+    fb=bb-1
+  end  
+  if d==2 then     --down
+    fb=bb+1
+  end  
+  if d==3 then     --left
+    fa=aa-1
+  end  
+  if d==4 then     --right
+    fa=aa+1
+  end
+  
+  if fa>=0 and fa<=5 and fb>=0 and fb<=5 then
+  	if a[fa][fb]~=2 then
+  		return true
+    else
+        return false
+    end
+  else
+    return false
+  end        		
+
+end
+
 function qlearn()
   x=3
   y=2
   t=0
-  for iteration=1,100 do
+ for epi=1,300 do
+  for iteration=1,6000 do
+  	ne[x][y]=ne[x][y]+1
+   if ne[x][y]>=20 then	
     local tempmax={}
     for k=1,4 do
       tempmax[k]=q[x][y][k]
@@ -93,15 +134,36 @@ function qlearn()
     local sum=0
     local dirary={}
     for k=1,4 do
-    	if q[x][y][k]==maxval then
+    	if q[x][y][k]==maxval and notwall(x,y,k) then
     		sum=sum+1
             dirary[sum]=k
         end
     end
-    dir=dirary[math.random(sum)]        
-    step(dir)
-  end  
+    dir=dirary[math.random(sum)]    
+    print(dir)
+   else
+    local tempmax={}
+    tempmax[1]=trueval(a[x][y-1])
+    tempmax[2]=trueval(a[x][y+1])
+    tempmax[3]=trueval(a[x-1][y])
+    tempmax[4]=trueval(a[x+1][y])
+    local maxval=math.max(unpack(tempmax))
+    local sum=0
+    local dirary={}
+    for k=1,4 do
+    	if tempmax[k]==maxval and notwall(x,y,k) then
+    		sum=sum+1
+            dirary[sum]=k
+        end
+    end
 
+    dir=dirary[math.random(sum)]    
+    print(dir)     
+   end     
+    step(dir)
+    printq()
+  end  
+ end
 end
 
 function step(dir)
@@ -109,19 +171,19 @@ function step(dir)
   local fx=x
   local fy=y
   if dir==1 then     --up
-    local point=q[x][y-1]
+    point=q[x][y-1]
     fy=y-1
   end  
-  if dir==2 then     --up
-    local point=q[x][y+1]
+  if dir==2 then     --down
+    point=q[x][y+1]
     fy=y+1
   end  
-  if dir==3 then     --up
-    local point=q[x-1][y]
+  if dir==3 then     --left
+    point=q[x-1][y]
     fx=fx-1
   end  
-  if dir==4 then     --up
-    local point=q[x+1][y]
+  if dir==4 then     --right
+    point=q[x+1][y]
     fx=fx+1
   end  
   
@@ -129,12 +191,36 @@ function step(dir)
   	tempary[i]=point[i]
   end 	
   local maxOfT = math.max(unpack(tempary))
-  q[x][y][dir]=q[x][y][dir]+(60/(59+t))*(a[i][j]+0.99*maxOfT-q[x][y][dir])
+  q[x][y][dir]=q[x][y][dir]+(600/(599+t))*(trueval(a[x][y])+0.99*maxOfT-q[x][y][dir])
 
   x=fx
   y=fy
   t=t+1
 end
+
+
+function printq()
+	print()
+	for i=0,5 do
+  		local temp = ""
+  		for j=0,5 do
+  			max=-100
+  			for k=1,4 do
+  				if max<q[i][j][k] then
+  					max=q[i][j][k]
+  	    		end
+  			end    		
+    		temp=temp..string.format("%.2f",tostring(max)).." "
+  		end
+  		print(temp)
+	end  
+end
+
+
+
+
+
+
 --main	
 f="mp4.1input.txt"
 getFile(f)
@@ -157,17 +243,5 @@ for i=0,5 do
   end
   print(temp)
 end
-print()
-for i=0,5 do
-  local temp = ""
-  for j=0,5 do
-  	max=-100
-  	for k=1,4 do
-  		if max<q[i][j][k] then
-  			max=q[i][j][k]
-  	    end
-  	end    		
-    temp=temp..string.format("%.2f",tostring(max)).." "
-  end
-  print(temp)
-end  
+
+qlearn()
